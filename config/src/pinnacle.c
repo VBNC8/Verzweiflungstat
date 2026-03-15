495370f8a4eb2ffc76a120cd8aa7dcf9caa9cbb9
VBNC8 <peter.greb@web.de>
2026-03-15 18:20:28 +0100

    Update pinnacle.c

--- config/src/pinnacle.c
+++ config/src/pinnacle.c
@@ -1,6 +1,13 @@
-// Minimaler Dummy-Treiber
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
+#include <zephyr/devicetree.h>
 
-static int pinnacle_init(const struct device *dev) { return 0; }
-DEVICE_DT_DEFINE(ANY, pinnacle_init, NULL, NULL, NULL, POST_KERNEL, 90, NULL);
+// Wir holen uns die ID direkt aus dem DeviceTree Label
+#define TRACKPAD_NODE DT_NODELABEL(trackpad)
+
+static int pinnacle_init(const struct device *dev) {
+    return 0;
+}
+
+// Hier nutzen wir jetzt die korrekte DT-Instanz
+DEVICE_DT_DEFINE(TRACKPAD_NODE, pinnacle_init, NULL, NULL, NULL, POST_KERNEL, 90, NULL);
