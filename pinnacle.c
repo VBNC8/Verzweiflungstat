5a404348c43c5cf0ce4270a4c996081698d47cde
VBNC8 <peter.greb@web.de>
2026-03-17 00:03:23 +0100

    Rettung

---
+++ pinnacle.c
@@ -0,0 +1,12 @@
+description: "Cirque Pinnacle Trackpad driver binding"
+compatible: "cirque,pinnacle"
+include: spi-device.yaml
+
+properties:
+  dr-gpios:
+    type: phandle-array
+    required: true
+    description: "Data Ready GPIO pin"
+  sideband-interupt:
+    type: boolean
+    description: "Use a sideband interrupt instead of SPI MISO"
